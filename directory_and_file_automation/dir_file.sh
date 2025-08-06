45d2a6fc06fb59d6be54dc8e5db7f099a181df7b
chomba-mumba <chombaibex@gmail.com>
2025-08-06 20:24:40 +0100

    finished creating folders and writing file

--- directory_and_file_automation/dir_file.sh
+++ directory_and_file_automation/dir_file.sh
@@ -11,9 +11,16 @@ echo "creating sub-directories..."
 
 cd $NEW_DIR
 
-subDirectories=(src docs tests bin)
+subDirectories=(src docs tests bin var)
 
 for sd in ${subDirectories[@]}; do
     mkdir $sd
     echo "created sub directory {$sd}"
 done
+
+#create log file
+sudo touch /var/output.log
+
+ts=$(date +"%Y-%m-%d %H:%M:%S")
+
+sudo echo "${ts} learning to write bash" >> ./var/output.log
